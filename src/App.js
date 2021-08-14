7ad9bf4100f3cb886ac2a54147f1e421290e1b6b
helketari <helketari@comcast.net>
2021-08-14 12:17:42 -0400

    Installed bootstrap and started working on HTML

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="weatherWrapper">
+          <Search />
+        </div>
+        <footer>
+          <a
+            href="https://github.com/helketari/vanilla-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Jessica Brosvic
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
